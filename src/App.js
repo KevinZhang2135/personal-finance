971f2ee53941bfc1bf0be73d242f41962ba53e71
Kevin <kevinbczhang@gmail.com>
2024-05-07 00:18:02 -0400

    Material Paper

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import Footer from "./components/Footer.jsx";
 const App = () => {
 	// $15,000 is the minimum wage salary set by US as of 2023
 	let [salary, setSalary] = useState(15000);
-	let [taxes, setTaxes] = useState([
+	let [taxes, setTaxes] = useState(
 		{
-			federal: 0,
+			federal: calculateTax(salary),
 			state: 0,
 			socialSecurityMedicaid: 0,
 		},
-	]);
+	);
 
 	return (
 		<div>
@@ -41,4 +41,33 @@ const App = () => {
 	);
 };
 
+const calculateTax = (annualSalary) => {
+    const brackets = {
+        // lower bracket: tax rate
+        0: 0.1,
+        11000: 0.12,
+        44725: 0.22,
+        95375: 0.24,
+        182100: 0.32,
+        231250: 0.35,
+        578125: 0.37,
+    };
+
+    // $13850 is the tax exemption with a single filing status
+    annualSalary = Math.max(0, annualSalary - 13850);
+
+    let totalTax = 0;
+    for (let [key, taxRate] of Object.entries(brackets).reverse()) {
+        const bracket = parseInt(key);
+
+        if (annualSalary > bracket) {
+            totalTax += (annualSalary - bracket) * taxRate;
+            annualSalary -= annualSalary - bracket;
+        }
+    }
+
+    return totalTax;
+};
+
 export default App;
+export {calculateTax};
--- src/components/Taxes.jsx
+++ src/components/Taxes.jsx
@@ -1,100 +1,92 @@
 import React from "react";
 import {
-	FormControl,
-	FilledInput,
-	InputAdornment,
-	InputLabel,
+    FormControl,
+    FilledInput,
+    InputAdornment,
+    InputLabel,
+    Paper,
 } from "@mui/material";
 
+import { calculateTax } from "../App";
+
 const Taxes = (props) => {
-	let { salary, setSalary, taxes, setTaxes } = props;
+    let { salary, setSalary, taxes, setTaxes } = props;
 
-	return (
-		<div className="container-fluid">
-			<div className="container-row">
-				<div className="container-col col-2">
-					<h2>Gross Annual Salary</h2>
-					<FormControl variant="filled">
-						<InputLabel htmlFor="filled-adornment-amount">
-							Gross Annual Salary
-						</InputLabel>
+    return (
+        <div className="container-fluid">
+            <div className="container-row">
+                <Paper
+                    className="container-col col-2"
+                    elevation={3}
+                    sx={{ margin: "0vh 1vw" }}
+                >
+                    <h2>Gross Annual Salary</h2>
+                    <FormControl variant="filled">
+                        <InputLabel
+                            htmlFor="filled-adornment-amount"
+                            sx={{ fontSize: "2vmin" }}
+                        >
+                            Gross Annual Salary
+                        </InputLabel>
 
-						<FilledInput
-							id="filled-adornment-amount"
-							startAdornment={
-								<InputAdornment position="start">
-									$
-								</InputAdornment>
-							}
-							defaultValue="15000"
-							placeholder="Enter income"
-							onChange={(e) => {
-								const annualSalary = parseInt(e.target.value);
-								setSalary(annualSalary);
-								setTaxes({
-									federal: calculateTax(annualSalary),
-								});
-							}}
-						/>
-					</FormControl>
+                        <FilledInput
+                            id="filled-adornment-amount"
+                            startAdornment={
+                                <InputAdornment
+                                    position="start"
+                                    sx={{ fontSize: "2vmin" }}
+                                >
+                                    $
+                                </InputAdornment>
+                            }
+                            sx={{ fontSize: "2vmin" }}
+                            defaultValue="15000"
+                            placeholder="Enter income"
+                            onChange={(e) => {
+                                const annualSalary = parseInt(e.target.value);
+                                setSalary(annualSalary);
+                                setTaxes({
+                                    federal: calculateTax(annualSalary),
+                                });
+                            }}
+                        />
+                    </FormControl>
 
-					<h2>Gross Monthly Salary</h2>
-					<p>${Math.round(salary / 12)}</p>
+                    <h2>Taxable Salary</h2>
+                    <p>${salary - 13850}</p>
 
-					<h2>Annual Federal Tax</h2>
-					<p>${Math.round(taxes.federal)}</p>
-				</div>
+                    <h2>Gross Monthly Salary</h2>
+                    <p>${Math.round(salary / 12)}</p>
 
-				<div className="container-col col-2 khaki-border">
-					<p>
-						The US implements a progressive tax that steadily takes
-						a larger share of the gross salary as income increases.
-						While many other factors such as filing-status and
-						number of children influence the total federal tax paid,
-						all approximations will be based on a single filing
-						status, so $13850 of the annual income is exempt from taxation.
-						<br />
-						<br />
-						Tax rates were calculated using the 2023 tax brackets
-						released by the IRS from{" "}
-						<a href="https://www.irs.gov/filing/federal-income-tax-rates-and-brackets">
-							www.irs.gov/filing/federal-income-tax-rates-and-brackets
-						</a>
-						.
-					</p>
-				</div>
-			</div>
-		</div>
-	);
-};
+                    <h2>Annual Federal Tax</h2>
+                    <p>${Math.round(taxes.federal)}</p>
+                </Paper>
 
-const calculateTax = (annualSalary) => {
-	const brackets = {
-		// lower bracket: tax rate
-		0: 0.1,
-		11000: 0.12,
-		44725: 0.22,
-		95375: 0.24,
-		182100: 0.32,
-		231250: 0.35,
-		578125: 0.37,
-	};
-
-    // $13850 is the 
-	annualSalary = annualSalary - 13850 > 0 ? annualSalary - 13850 : 0;
-
-	let totalTax = 0;
-	for (let [key, taxRate] of Object.entries(brackets).reverse()) {
-		const bracket = parseInt(key);
-
-		if (annualSalary > bracket) {
-			console.log(annualSalary - bracket, taxRate);
-			totalTax += (annualSalary - bracket) * taxRate;
-			annualSalary -= annualSalary - bracket;
-		}
-	}
-
-	return totalTax;
+                <Paper
+                    className="container-col col-2"
+                    elevation={3}
+                    sx={{ margin: "0vh 1vw" }}
+                >
+                    <p>
+                        The US implements a progressive tax that steadily takes
+                        a larger share of the gross salary as income increases.
+                        While many other factors such as filing-status and
+                        number of children influence the total federal tax paid,
+                        all approximations will be based on a single filing
+                        status.
+                        <br />
+                        <br />
+                        Tax rates were calculated using the 2023 tax brackets
+                        released by the IRS from{" "}
+                        <a href="https://www.irs.gov/filing/federal-income-tax-rates-and-brackets">
+                            www.irs.gov/filing/federal-income-tax-rates-and-brackets
+                        </a>
+                        .
+                    </p>
+                </Paper>
+            </div>
+        </div>
+    );
 };
 
 export default Taxes;
