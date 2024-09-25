45a1ce48c595a8d8d92945af10ab64f32020493d
Kevin Zhang <kevinzhang@wireless-10-104-10-3.umd.edu>
2024-09-25 09:38:05 -0400

    Title Separation from Content and Forms

--- src/components/food/FoodInfo.jsx
+++ src/components/food/FoodInfo.jsx
@@ -4,35 +4,38 @@ import FoodForm from "./FoodForm";
 
 const FoodInfo = (props) => {
     return (
-        <Stack
+        <Box
             id="food-info"
             className="content-container"
             bgcolor="beige.main"
-            direction={{ xs: "column", lg: "row" }}
             spacing={4}
             py="5vh"
         >
-            <Box width={{ xs: 1, lg: 0.5 }}>
-                <Typography variant="h1" gutterBottom>
-                    Food
-                </Typography>
-        
-                <Typography variant="p" display="block" gutterBottom>
-                    Recently, food costs have inflated due to a variety of
-                    disruptions in the food supply chain. Due to animal and
-                    plant diseases, weather, the Russo-Ukrainian War, and
-                    numerous other factors, food prices in 2023 are 25% higher
-                    than that in 2018.<sup>14</sup>
-                </Typography>
-                <Typography variant="p">
-                    According, budgeting for food expenses becomes ever more
-                    important, especially considering that US consumers spend
-                    about 12.8% of their total household expenditures on food in
-                    2022.<sup>15</sup>
-                </Typography>
-            </Box>
-            <FoodForm {...props} />
-        </Stack>
+            <Typography variant="h1" gutterBottom>
+                Food
+            </Typography>
+
+            <Stack direction={{ xs: "column", lg: "row" }} spacing={4}>
+                <Box width={{ xs: 1, lg: 0.5 }}>
+                    <Typography variant="p" display="block" gutterBottom>
+                        Recently, food costs have inflated due to a variety of
+                        disruptions in the food supply chain. Due to animal and
+                        plant diseases, weather, the Russo-Ukrainian War, and
+                        numerous other factors, food prices in 2023 are 25% higher
+                        than that in 2018.<sup>14</sup>
+                    </Typography>
+                    
+                    <Typography variant="p">
+                        According, budgeting for food expenses becomes ever more
+                        important, especially considering that US consumers spend
+                        about 12.8% of their total household expenditures on food in
+                        2022.<sup>15</sup>
+                    </Typography>
+                </Box>
+
+                <FoodForm {...props} />
+            </Stack>
+        </Box>
     );
 };
 
--- src/components/healthInsurance/HealthInsuranceInfo.jsx
+++ src/components/healthInsurance/HealthInsuranceInfo.jsx
@@ -4,57 +4,59 @@ import HealthInsuranceForm from "./HealthInsuranceForm";
 
 const HealthInsuranceInfo = (props) => {
     return (
-        <Stack
+        <Box
             id="health-insurance-info"
             className="content-container"
-            direction={{ xs: "column", lg: "row" }}
             spacing={4}
             pt="5vh"
             pb={4}
         >
-            <Box width={{ xs: 1, lg: 0.5 }}>
-                <Typography variant="h1" gutterBottom>
-                    Health Insurance
-                </Typography>
-                <Typography variant="p" display="block" gutterBottom>
-                    Injuries and illnesses are inevitable in life. Health
-                    insurance helps alleviate some of the burden of paying
-                    medical bill which are notoriously expensive in the US.
-                    Before subscribing to an insurance plan, it may be astute to
-                    become familiar with how such services work.
-                </Typography>
+            <Typography variant="h1" gutterBottom>
+                Health Insurance
+            </Typography>
 
-                <Typography variant="p" display="block" gutterBottom>
-                    <b>Premiums</b> are monthly fees paid to insurance in
-                    exchange for coverage of potential healthcare costs.
-                </Typography>
-                <Typography variant="p" display="block" gutterBottom>
-                    <b>Deductibles</b> are the amount of money subscribers must
-                    pay before insurance begins to share a portion of medical
-                    fees.
-                </Typography>
-                <Typography variant="p" display="block" gutterBottom>
-                    <b>Out-of-Pocket Max</b> is the amount of money subscribers
-                    are required to pay before insurance covers all healthcare
-                    costs.
-                </Typography>
+            <Stack direction={{ xs: "column", lg: "row" }} spacing={4}>
+                <Box width={{ xs: 1, lg: 0.5 }}>
+                    <Typography variant="p" display="block" gutterBottom>
+                        Injuries and illnesses are inevitable in life. Health
+                        insurance helps alleviate some of the burden of paying
+                        medical bill which are notoriously expensive in the US.
+                        Before subscribing to an insurance plan, it may be astute to
+                        become familiar with how such services work.
+                    </Typography>
 
-                <Typography variant="p" display="block" gutterBottom>
-                    Oftentimes, insurance plans with low premiums have higher
-                    deductible thresholds to compensate. Thus, it may be
-                    preferable for those who are healthy and do not expect to
-                    visit the doctor frequently.
-                </Typography>
-                <Typography variant="p" display="block" mb={4}>
-                    Conversely, other plans may charge relatively higher
-                    premiums for lower deductibles which is favorable for older
-                    adults or those who use medical services regularly.
-                    <sup>16</sup>
-                </Typography>
-            </Box>
+                    <Typography variant="p" display="block" gutterBottom>
+                        <b>Premiums</b> are monthly fees paid to insurance in
+                        exchange for coverage of potential healthcare costs.
+                    </Typography>
+                    <Typography variant="p" display="block" gutterBottom>
+                        <b>Deductibles</b> are the amount of money subscribers must
+                        pay before insurance begins to share a portion of medical
+                        fees.
+                    </Typography>
+                    <Typography variant="p" display="block" gutterBottom>
+                        <b>Out-of-Pocket Max</b> is the amount of money subscribers
+                        are required to pay before insurance covers all healthcare
+                        costs.
+                    </Typography>
 
-            <HealthInsuranceForm {...props} />
-        </Stack>
+                    <Typography variant="p" display="block" gutterBottom>
+                        Oftentimes, insurance plans with low premiums have higher
+                        deductible thresholds to compensate. Thus, it may be
+                        preferable for those who are healthy and do not expect to
+                        visit the doctor frequently.
+                    </Typography>
+                    <Typography variant="p" display="block" mb={4}>
+                        Conversely, other plans may charge relatively higher
+                        premiums for lower deductibles which is favorable for older
+                        adults or those who use medical services regularly.
+                        <sup>16</sup>
+                    </Typography>
+                </Box>
+
+                <HealthInsuranceForm {...props} />
+            </Stack>
+        </Box>
     );
 };
 
